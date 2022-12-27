5a22aedf0dfdbe537d256098f539f9a2d5d6ce53
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-12-27 14:25:34 -0500

    first commit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './feature.css'
+
+const Features = () => {
+    return (
+        <div>
+            Feature
+        </div>
+    );
+};
+
+export default Features;
---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const footer = () => {
+    return (
+        <div>
+            <h2>jej</h2>
+        </div>
+    );
+};
+
+export default footer;
